50a305a0d116adc22af34a91851fa5a6f17e4cd9
jonfon07 <jonfon07@yahoo.com>
2017-04-10 09:12:04 -0400

    animals js file

---
+++ show.js
@@ -0,0 +1,43 @@
+function showhide()
+ {
+       var div = document.getElementById("malt");
+if (div.style.display !== "none") {
+    div.style.display = "none";
+}
+else {
+    div.style.display = "block";
+}
+ }
+
+function showhide1()
+ {
+       var div = document.getElementById("Bear");
+if (div.style.display !== "none") {
+    div.style.display = "none";
+}
+else {
+    div.style.display = "block";
+}
+ }
+
+function showhides()
+ {
+       var div = document.getElementById("spnx");
+if (div.style.display !== "none") {
+    div.style.display = "none";
+}
+else {
+    div.style.display = "block";
+}
+ }
+
+function showhide2()
+ {
+       var div = document.getElementById("cock");
+if (div.style.display !== "none") {
+    div.style.display = "none";
+}
+else {
+    div.style.display = "block";
+}
+ }
