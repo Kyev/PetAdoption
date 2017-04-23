3805781e1d31db3626e8d4bbc1bc5f659523a475
asaifbutt <asaifaamir@hotmail.com>
2017-04-23 18:10:09 -0400

    Change addPet.php

--- addPet.php
+++ addPet.php
@@ -1,123 +1,110 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <title>Pets'R'Us</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="../css/animals.css">
-</head>
-
-<body>
-<div class="header">
-    <div class="container">
-        <ul>
-            <li class="main">
-                <a href="#"><img src="../img/Pets_R_Us_sml.png" alt="Logo" id="logo"></a>
-            </li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="animals.html">Animals</a></li>
-            <li><a href="../">Logout</a></li>
-        </ul>
-    </div>
-</div>
-
-<!--
-< <div class="jumbotron">-->
-<div class="container">
-    <div class="row">
-        <h3>New Pet</h3>
-        <div class="col-sm-4">
-            <div class="side_wrapper">
-                <form action="addPet.php" method="post" name="contactform">
-                    <p>Pet ID *</p>
-                    <p><input maxlength="50" name="pet_id" size="30" type="text" /></p>
-                    <p>Pet Name *</p>
-                    <p><input maxlength="50" name="pet_name" size="30" type="text" /></p>
-                    <p>Pet Breed *</p>
-                    <p><input maxlength="50" name="pet_breed" size="30" type="text" /></p>
-                    <p>Pet Type</p>
-                    <p><input maxlength="30" name="pet_type" size="30" type="text" /></p>
-                    <p>Pet Sex *</p>
-                    <p><input maxlength="80" name="pet_sex" size="30" type="text" /></p>
-                    <p>Pet Weight *</p>
-                    <p><input maxlength="30" name="pet_weight" size="30" type="text" /></p>
-                    <p>Pet Description *</p>
-                    <p><textarea cols="25" maxlength="1000" name="pet_description" rows="6"></textarea></p>
-                    <p><input type="submit" value="Submit" /></p>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
 <?php
-//This gets all the other information from the form
-$id=$_POST['pet_id'];
-$name=$_POST['pet_name'];
-$type=$_POST['pet_type'];
-$breed=$_POST['pet_breed'];
-$sex=$_POST['pet_sex'];
-$weight=$_POST['pet_weight'];
-$description=$_POST['pet_description'];
-
-// Connects to your Database
-require_once '../php/db_connect.php';
-
-//Writes the information to the database
-$sql = "INSERT INTO PETS (id, name, breed, type, sex, weight_in_lbs, description) VALUES ('$id', '$name', '$breed', '$type', '$sex', '$weight', '$description') ";
-$result=$db->query($sql);
-
-
-if($result->num_rows > 0) {
-echo '        <div class="alert alert-success">Pet Added Successfully.</div>' . PHP_EOL;
-} else {
-echo '        <div class="alert alert-danger">Error: (' . $db->errno . ') ' . $db->error . '</div>' . PHP_EOL;
-exit(); // Prevents the rest of the file from running
-}
-
+//    include 'php/dbh.inc.php';
+    include 'php/pet.inc.php';
+//    include 'addpets.php';
 ?>
 
-<div class="supporting">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <!--  <h2>Search</h2>
-        <p>Find a pet based on location, type, breed, age, and gender. We look through hundreds of shelters, agencies, and rescue sites.</p>
-        <a href="#" class="btn">Go</a>-->
-            </div>
-            <div class="col-md-4">
-                <!-- <h2>Learn</h2>
-        <p>Read articles on feeding, training, grooming, and more. Our resources ensure that you are fully prepared as a new pet owner.</p>
-        <a href="#" class="btn">Go</a>-->
-            </div>
-            <div class="col-md-4">
-                <!--   <h2>Help</h2>
-        <p>Get involved to help pets in need. You can make a difference through volunteering, social media, and donations.</p>
-        <a href="#" class="btn">Go</a>-->
-            </div>
-        </div>
-    </div>
-</div>
+<!DOCTYPE html>
+<html lang='en-US'>
+<header>
+    <meta charset='UTF-8'>
+    <title>Pet</title>
+</header>
+<body>
 
-<div class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-2">
-                <div class="copy">© Pets'R'Us</div>
-            </div>
-            <div class="col-md-10">
-                <ul class="nav nav-pills pull-right">
-                    <li><a href="#">FAQ</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </div>
-        </div>
+
+    <h1>Add Pet</h1>
+    <div>
+        <form method="POST">
+            <input type="text" name="name" placeholder="Pet name">
+
+            <select name="type">
+                <option value="Dog">Dog</option>
+                <option value="Cat">Cat</option>
+            </select>
+            <select name="breed">
+                <option>Breed</option>
+                <option value="Pitbull">Pitbull</option>
+                <option value="Cavapoo">Cavapoo</option>
+            </select>
+            <select name="sex">
+                <option value="Male">Male</option>
+                <option value="Female ">Female</option>
+            </select>
+            <select name="size">
+                <option value="Small">Small</option>
+                <option value="Medium">Medium</option>
+                <option value="Large">Large</option>
+            <select name="status">
+                <option value="Available">Available</option>
+                <option value="Reserved">Reserved</option>
+            </select>
+
+            <input type="text" name="description" placeholder="Pet descprition">
+
+            <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
     </div>
-</div>
+
+    <!--<h1>Search Pet</h1>
+    <div>
+        <form method="POST">
+            <select name="type">
+                <option value="Dog">Dog</option>
+                <option value="Cat">Cat</option>
+            </select>
+            <!--<select name="breed">
+                <option>Breed</option>
+                <option value="Pitbull">Pitbull</option>
+                <option value="Cavapoo">Cavapoo</option>
+            </select>
+            <select name="sex">
+                <option value="Male">Male</option>
+                <option value="Female ">Female</option>
+            </select>
+            <select name="size">
+                <option value="Small">Small</option>
+                <option value="Medium">Medium</option>
+                <option value="Large">Large</option>
+            </select>-->
+            <button type="submit" name="search">Search</button>
+        </form>
+    </div>-->
+
+    <?php
+
+        $name = $_POST['name'];
+        $type = $_POST['type'];
+        $breed = $_POST['breed'];
+        $sex = $_POST['sex'];
+        $description = $_POST['description'];
+        $size = $_POST['size'];
+        $status = $_POST['status'];
+
+        $add = new Pet();
+        $add->addPet($name, $breed, $type, $sex, $size, $status, $description);
+
+        /*$type = $_POST['type'];
+
+        $search = new Pet();
+        $search->getPets($type);*/
+
+    ?>
+
+
 </body>
-
 </html>
 
+
+
+
+
+
+
+
+
+
+
+
+
+<!--INSERT INTO `PETS` (`pid`, `pName`, `pBreed`, `pType`, `pSex`, `pStatus`, `pDescription`, `pSize`) VALUES (NULL, 'Luke', 'Kylo', 'Dog', 'Male', 'Available', 'Random fits of rage.', 'Small');-->
