753fd8be16157b308312339b948b959fab377c3d
Max <shashkunm@gmail.com>
2017-04-30 22:40:35 -0400

    index footer fix

--- php/register.php
+++ php/register.php
@@ -47,27 +47,18 @@ input[type=checkbox].glyphicon{
 </head>
 
 <body>
-  <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="../index.html"><img src="../img/Pets_R_Us_sml.png" alt="Logo"></a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
+  <div class="header">
+    <div class="container">
+        <ul>
+            <li class="main">
+                <a href="../index.html"><img src="../img/Pets_R_Us_sml.png" alt="Logo" id="logo"></a>
+            </li>
             <li><a href="animals.php">Animals</a></li>
-            <li><a href="register.php">Register</a></li>
-            <li><a href="html/login.html">Login</a></li>
-          </ul>
-        </div>
-        <!--/.nav-collapse -->
-      </div>
-    </nav>
+            <li><a href="../php/register.php">Register</a></li>
+            <li><a href="../html/login.html">Login</a></li>
+        </ul>
+    </div>
+</div>
 
 
   <div class="supporting">
@@ -167,19 +158,7 @@ input[type=checkbox].glyphicon{
   </div>
 
   <div class="footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-2">
-          <div class="copy">© Pets'R'Us</div>
-        </div>
-        <div class="col-md-10">
-          <ul class="nav nav-pills pull-right">
-            <li><a href="#">FAQ</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+     <div class="copy">© Pets'R'Us</div>
   </div>
 </body>
 
