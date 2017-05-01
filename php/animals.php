e3018fa981d3eecad8de60c4f81a454d15c71e61
Max <shashkunm@gmail.com>
2017-04-30 22:00:36 -0400

    changed headers....

--- php/animals.php
+++ php/animals.php
@@ -19,27 +19,18 @@
 </head>
 
 <body>
-<nav class="navbar navbar-default navbar-fixed-top">
+<div class="header">
     <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="../index.html"><img src="../img/Pets_R_Us_sml.png" alt="Logo"></a>
-      </div>
-      <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="animals.php">Animals</a></li>
-          <li><a href="register.php">Register</a></li>
-          <li><a href="../html/login.html">Login</a></li>
+        <ul>
+            <li class="main">
+                <a href="#"><img src="../img/Pets_R_Us_sml.png" alt="Logo" id="logo"></a>
+            </li>
+            <li><a href="animals.php">Animals</a></li>
+            <li><a href="../php/register.php">Register</a></li>
+            <li><a href="login.html">Login</a></li>
         </ul>
-      </div>
-      <!--/.nav-collapse -->
     </div>
-  </nav>
+</div>
 <!--
 < <div class="jumbotron">-->
 <div class="container">
