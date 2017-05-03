2c134f637f6e90b635234668db9fd384d3549f97
dymasc <dcoelho@fau.edu>
2017-05-03 12:48:49 -0400

    Update addPet.php

--- php/addPet.php
+++ php/addPet.php
@@ -53,7 +53,7 @@ input[type=checkbox].glyphicon{
         <li class="main"><a href="../index.html"><img src="../img/Pets_R_Us_sml.png" alt="Logo"></a></li>
         <li><a href="../html/about.html">About</a></li>
         <li><a href="../html/animals.html">Animals</a></li>
-        <li><a href="#">Register</a></li>
+        <li><a href="../php/register.php">Register</a></li>
           <li><a href="../html/login.html">Login</a></li>
       </ul>
     </div>
